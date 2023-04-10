6be2388362d08ff5502a8d631d40922f6c8b5ad9
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-04-09 22:28:13 -0400

    Return correct reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -19,7 +19,7 @@ const pool = new Pool({
  */
 const getUserWithEmail = function(email) {
   return pool
-    .query(`SELECT email, password FROM users
+    .query(`SELECT * FROM users
   WHERE email = $1`, [email])
     .then(result => {
       if (!email) {
@@ -79,7 +79,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT reservations.*, properties.*, avg(rating) as average_rating
+ FROM reservations
+ JOIN properties ON reservations.property_id = properties.id
+ JOIN property_reviews ON properties.id = property_reviews.property_id
+ WHERE reservations.guest_id = $1
+ GROUP BY properties.id, reservations.id
+ ORDER BY reservations.start_date
+ LIMIT $2;`, [guest_id, limit])
+    .then(result => {
+      return result.rows
+    }
+    )
+    .catch((err) => {
+      console.log('error:', err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
